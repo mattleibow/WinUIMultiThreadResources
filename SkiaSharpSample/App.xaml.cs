d318a491ed18af845a25aea8b66ef7ffa3a12a93
Matthew Leibowitz <mattleibow@live.com>
2022-04-24 12:34:41 +0800

    Add project files.

---
+++ SkiaSharpSample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace SkiaSharpSample
+{
+	public partial class App : Application
+	{
+		private Window window;
+
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnLaunched(LaunchActivatedEventArgs args)
+		{
+			if (window != null)
+				return;
+
+			Resources.MergedDictionaries.Add(new XamlControlsResources());
+
+			window = new MainWindow();
+			window.Activate();
+		}
+	}
+}
---
+++ SkiaSharpSample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using Microsoft.UI.Xaml;
+
+namespace SkiaSharpSample
+{
+	public sealed partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var thread = new Thread(() =>
+			{
+				Application.Start((p) =>
+				{
+					var dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+					var syncContext = new Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(dispatcherQueue);
+					SynchronizationContext.SetSynchronizationContext(syncContext);
+
+					var w = new MainWindow();
+					w.Activate();
+				});
+			});
+
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+		}
+	}
+}
